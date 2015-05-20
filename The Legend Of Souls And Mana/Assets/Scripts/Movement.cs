2dffff23a5fc6bc48572fe91747eb8ccdfd78133
Championnick <nick_diktakis@live.ca>
2015-05-19 22:32:54 -0400

    Fixed movement

--- The Legend Of Souls And Mana/Assets/Scripts/Movement.cs
+++ The Legend Of Souls And Mana/Assets/Scripts/Movement.cs
@@ -4,23 +4,33 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 
 	Vector3 move;
-	float speed;
+	public float speed = 5.0f;
+	float timer;
+	public float timeToRun = 0.5f;
 
 	// Use this for initialization
 	void Start () {
-		speed = 5.0f;
+		timer = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+
 		move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
 		transform.position += move * speed * Time.deltaTime;
 
-		if (Input.GetButton("Fire1"))
-			speed = 0.05f;
-		else {
+		if (Input.GetButton ("Fire1")){
+			timer += Time.deltaTime;
+			print (timer);
+			if (timer > timeToRun){
+				speed = 8.0f;
+			}
+
+		}
+		else{
 			speed = 5.0f;
+			timer = 0.0f;
 		}
 
 	}
