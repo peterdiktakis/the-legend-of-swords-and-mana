f7ed5a1e7475a795dcfc7b43c01204b6778f0e59
Championnick <nick_diktakis@live.ca>
2015-05-20 00:48:10 -0400

    Fixed Rolling (Complete)
    -Can tweak rollTimer to equate diagonal rolling with horizontal/vertical
    rolling

--- The Legend Of Souls And Mana/Assets/Scripts/Movement.cs
+++ The Legend Of Souls And Mana/Assets/Scripts/Movement.cs
@@ -6,18 +6,22 @@ public class Movement : MonoBehaviour {
 	//Character variables
 
 	public float timeToRun = 0.5f;
-	public float rollSpeedModifier = 10.2f;
+	public float timeToRoll = 0.2f;
+	public float rollSpeedModifier = 0.05f;
 	public float speed = 5.0f;
 
 	Vector3 move;
 	Vector3 direction;
+	Vector3 rollDirection;
 	
-	float timer;
+	float runTimer;
+	float rollTimer;
 	bool isRolling;
 
 	// Use this for initialization
 	void Start () {
-		timer = 0.0f;
+		runTimer = 0.0f;
+		rollTimer = 0.0f;
 		isRolling = false;
 		direction = new Vector3 (0.0f, 0.0f, 0.0f);
 	}
@@ -25,9 +29,10 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		print (direction);
+		print (isRolling + "      "  + rollTimer);
 		move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
 		if (!isRolling) {
+			rollTimer = 0.0f;
 			transform.position += move * speed * Time.deltaTime;
 
 			if(Input.GetAxis ("Horizontal") == 0 && Input.GetAxis ("Vertical") == 0)
@@ -61,24 +66,34 @@ public class Movement : MonoBehaviour {
 				direction = new Vector3(-1.0f, -1.0f, 0.0f);
 			}
 
-
-
 			if (Input.GetButton ("Fire1")) {
-				timer += Time.deltaTime;
-				if (timer > timeToRun) {
+				runTimer += Time.deltaTime;
+				if (runTimer > timeToRun) {
 					speed = 8.0f;
 				}
 				
 			} else {
 				speed = 5.0f;
-				timer = 0.0f;
+				runTimer = 0.0f;
 			}
 
 			if (Input.GetButtonDown ("Fire2")) {
 				isRolling = true;
+				rollDirection = direction;
 			}
 		} else {
-			//transform.position += direction;
+			rollTimer += Time.deltaTime;
+			//For Diagonal Rolling
+			if(rollDirection.x == 0 || rollDirection.y == 0)
+				transform.position += rollDirection * 0.4f;
+			else //For 4-axis Rolling
+				transform.position += rollDirection * 0.25f;
+
+			if(rollTimer > timeToRoll){
+				isRolling = false;
+			}
+			transform.position += move * speed * Time.deltaTime;
+
 		}
 
 
